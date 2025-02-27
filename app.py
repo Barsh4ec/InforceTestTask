539fcf94211c746a4c110f470ef28e7dea887348
Taras <tarasborshch4@gmail.com>
2025-02-27 21:47:59 +0200

    Created basic models

---
+++ app.py
@@ -0,0 +1,39 @@
+
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session, relationship
+
+
+
+DATABASE_URL = "postgresql://user:password@db/lunch_voting"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+class Restaurant(Base):
+    __tablename__ = "restaurants"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    menus = relationship("Menu", back_populates="restaurant")
+
+class Menu(Base):
+    __tablename__ = "menus"
+    id = Column(Integer, primary_key=True, index=True)
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"))
+    date = Column(Date, default=date.today)
+    items = Column(String)
+    restaurant = relationship("Restaurant", back_populates="menus")
+
+class Vote(Base):
+    __tablename__ = "votes"
+    id = Column(Integer, primary_key=True, index=True)
+    employee_id = Column(Integer, ForeignKey("employees.id"))
+    menu_id = Column(Integer, ForeignKey("menus.id"))
+
